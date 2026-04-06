0b1033a5a60f5076024240b5ddf041a95e05bdf6
Helton Fernades Soares <soareshelton939@gmail.com>
2026-04-06 18:19:48 -0300

    exercise 2

---
+++ jogo.py
@@ -0,0 +1,29 @@
+from random import randint  
+ 
+while True:
+    escolha = input('Par ou Impar? [P/I]: ').upper()
+    if escolha not in ['P', 'I']:
+        print('Escolha inválida!')
+        continue
+    
+    jogador = int(input('Digite um valor (0-10): '))
+    if not 0 <= jogador <= 10:
+        print('Valor inválido!')
+        continue
+    
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = "PAR" if total % 2 == 0 else "IMPAR"
+    
+    print(f'Você jogou {jogador}, computador {computador}. Total {total} = {tipo}')
+    
+    if escolha == tipo:
+        print('Você GANHOU!')
+    else:
+        print('Você PERDEU!')
+    
+    continuar = input('Quer continuar? [S/N]: ').upper()
+    if continuar == 'N':
+        break
+
+print('Fim de jogo!')
